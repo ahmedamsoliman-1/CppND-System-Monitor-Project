cf89fdf3d969cb84dd381bcfc56ba2861a3c341b
root <root@AhmedAliMSoliman530S.localdomain>
2020-01-26 00:17:17 +0400

    system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -23,16 +23,20 @@ vector<Process>& System::Processes() { return processes_; }
 std::string System::Kernel() { return string(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() 
+{ return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() 
+{ return string(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() 
+{ return 0; }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() 
+{ return 0; }
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() 
