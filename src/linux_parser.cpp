9f6e8ea7b10704cb009dae6b095b39e9bcc304f8
root <root@AhmedAliMSoliman530S.localdomain>
2020-01-28 12:06:29 +0400

    d

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -193,7 +193,8 @@ float LinuxParser::Utilization()
     while (std::getline(filestream, line)) {
       std::istringstream linestream(line);
       while (linestream >> key >> value1 >> value2 >> value3 >> value4 >> value5 >> value6 >> value7 >> value8 >> value9 >> value10) {
-        if (key == "cpu") {
+        if (key == "cpu") 
+        {
           utilz = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10; 
           return utilz;
         }
