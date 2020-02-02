bd1443308c8e82d939ddd3444350ca6aa476c09c
Ahmed Ali M. Soliman <ahmed-soliman@hcl.com>
2020-02-02 14:54:20 +0400

    review comment and final commit after testin

--- src/system.cpp
+++ src/system.cpp
@@ -32,39 +32,20 @@ vector<Process>& System::Processes()
 }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() 
-{
-    return LinuxParser::Kernel();
-}
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() 
-{
-    return LinuxParser::MemoryUtilization();
-}
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() 
-{
-    return LinuxParser::OperatingSystem();
-}
+std::string System::OperatingSystem() { LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() 
-{
-    return LinuxParser::RunningProcesses();
-}
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() 
-{
-    return LinuxParser::TotalProcesses();
-    
-}
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() 
-{
-    return LinuxParser::UpTime();
-}
+long int System::UpTime() { return LinuxParser::UpTime(); }
 
